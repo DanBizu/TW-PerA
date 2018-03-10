c31c09a9e45a26751c510f11d76bf8b07795da7d
Bleoju Ionut Bogdan <ionutyop@gmail.com>
2018-03-10 18:20:47 +0200

    Home main table
    home main table responsiv to clicks

--- JS/Home.js
+++ JS/Home.js
@@ -2,19 +2,33 @@ var modalBtn = document.getElementsByClassName('activityContainerButton');
 
 var closeBtn = document.getElementsByClassName('modalClose');
 
+
+
 for (var i = 0; i < modalBtn.length; i++) {
 
   var thisBtn = modalBtn[i];
   var thisClose = closeBtn[i];
 
+
   thisBtn.onclick = function(){
     var modal = document.getElementById(this.dataset.modal);
     modal.style.display = "block";
+
+    window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
   }
+
 
   thisClose.onclick = function(){
     var modal = document.getElementById(this.dataset.modal);
     modal.style.display = "none";
   }
+
+
+
+
 
 }
