f90308a3d60974ed678678c7a7edb252fb46e0e4
Bleoju Ionut Bogdan <ionutyop@gmail.com>
2018-03-13 22:29:08 +0200

    Add + remove

--- JS/Home.js
+++ JS/Home.js
@@ -1,4 +1,4 @@
-var modalBtn = document.getElementsByClassName('activityContainerButton');
+var modalBtn = document.getElementsByClassName('activityName');
 
 var closeBtn = document.getElementsByClassName('modalClose');
 
@@ -25,8 +25,29 @@ for (var i = 0; i < modalBtn.length; i++) {
     modal.style.display = "none";
   }
 
+}
 
+function removeElement(parentDiv, childDiv){
+     if (childDiv == parentDiv) {
+          alert("The parent div cannot be removed.");
+     }
+     else if (document.getElementById(childDiv)) {
+          var child = document.getElementById(childDiv);
+          var parent = document.getElementById(parentDiv);
+          parent.removeChild(child);
+     }
+     else {
+          alert("Child div has already been removed or does not exist.");
+          return false;
+     }
+}
 
+function addElement() {
 
+    var p = document.getElementById('tableContainer1');
+    var newElement = document.createElement("DIV" , 'activityContainerButton1');
+    newElement.innerHTML = '<div class="activityContainerButton" , id="activityContainerButton1" ><table class="activityContainer" , id="activityContainer1" ><tr><th rowspan="2" , class="activityName" , data-modal="modalEdit1"> Tenis </th><td class="activityHour">17.00-19.00</td><th rowspan="2" , class="deleteButton"> <i class="fa fa-trash" , onclick="removeElement(\'tableContainer1',\'activityContainerButton1')""></i></th>	</tr><tr><td class="activityDate">23/06/2019</td></tr> </table>	</div>'
+      newElement.innerHTML.color="white";
+    p.appendChild(newElement);
 
 }
