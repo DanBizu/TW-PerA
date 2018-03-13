6a9f34c1f900837a56275c3112aa15246a1435c9
DanBizu <bizu.dan@gmail.com>
2018-03-13 20:21:08 +0200

    Friends js, images

---
+++ JS/Friends.js
@@ -0,0 +1,12 @@
+function Expand(){
+
+	var panel = document.getElementById("toBeExpanded");
+
+	if(panel.style.display === "block"){
+		panel.style.display = "none";
+	}
+	else{
+		panel.style.display = "block";
+	}
+
+}
