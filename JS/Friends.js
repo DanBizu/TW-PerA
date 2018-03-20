9148f0192dc0448841f5700a0de75598e6012bc6
DanBizu <bizu.dan@gmail.com>
2018-03-20 19:12:51 +0200

    Added activies table in Friends

--- JS/Friends.js
+++ JS/Friends.js
@@ -1,14 +1,17 @@
-function Expand(){
+var acc = document.getElementsByClassName("Friend");
+var table = document.getElementById("toBeExpanded");
+var i;
 
-	var panel = document.getElementById("toBeExpanded");
-
-	if(panel.style.display === "block"){
-		panel.style.display = "none";
-	}
-	else{
-		panel.style.display = "block";
-	}
-
+for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(){
+    	if(table.style.display == "none"){
+    		table.style.display = "block";
+    		this.className = "active";
+    	} else {
+    		table.style.display = "none";
+    		this.className = "";
+    	}
+    }
 }
 
 function ExpandSidebar(){
