d2ffefd2bb820bb3d025593b6c654d0e8489f2a0
Bleoju Ionut Bogdan <ionutyop@gmail.com>
2018-04-12 15:59:55 +0300

    Login + Register
    Login + register , responsive

---
+++ JS/Login.js
@@ -0,0 +1,13 @@
+function Transition(){
+    var login = document.getElementById("loginBox");
+    var register =  document.getElementById("registerBox");
+    login.classList.toggle('invisible');
+    register.classList.toggle('visible');
+}
+
+function reversTransition(){
+    var login = document.getElementById("loginBox");
+    var register =  document.getElementById("registerBox");
+    login.classList.toggle('visible');
+    register.classList.toggle('invisible');
+}
